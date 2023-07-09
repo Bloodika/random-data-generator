f6de114b2a2d29543c385dbf9d89ff8275de54e1
bloodika <bloodika@gmail.com>
2023-07-09 18:50:33 +0200

    init

---
+++ src/main/java/com/bloodika/randomdatagenerator/RandomDataGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.bloodika.randomdatagenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomDataGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandomDataGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bloodika/randomdatagenerator/RandomDataGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bloodika.randomdatagenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomDataGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
